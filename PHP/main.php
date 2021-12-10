772ce0c381a4ac0e41ea3828ed7d2f634392d6d3
Jacob Philpott <jacob.philpott@gmx.com>
2021-12-10 15:35:55 +0300

    PHP Update

--- PHP/main.php
+++ PHP/main.php
@@ -30,4 +30,30 @@
 
   print("$return_value\n");
 
+  class Fruit {
+
+    // Properties
+    public $name;
+    public $color;
+
+    // Methods
+    function set_name($name) {
+      $this->name = $name;
+    }
+
+    function get_name() {
+      return $this->name;
+    }
+
+  }
+
+  $apple = new Fruit();
+  $banana = new Fruit();
+
+  $apple->set_name("Apple");
+  $banana->set_name("Banana");
+
+  echo $apple->get_name() . "\n";
+  echo $banana->get_name() . "\n";
+
 ?>
